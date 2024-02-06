e31e2d7dcb8a57733e106c13ac439b2571d90010
Akash-Dutta-Dev-io <149801749+Akash-Dutta-Dev-io@users.noreply.github.com>
2024-02-06 20:53:05 +0530

    fix loading issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import { Link, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
